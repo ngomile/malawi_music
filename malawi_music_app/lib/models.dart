799363da358edb47f47e7a9f2d4b7fa87c0b2013
ngomile <ngomile932@gmail.com>
2022-04-17 17:20:18 +0200

    Added extra attributes to song model

--- malawi_music_app/lib/models.dart
+++ malawi_music_app/lib/models.dart
@@ -2,29 +2,39 @@ class Song {
   /// [Song] stores common attributes of songs which can later
   /// be accessed to use the attributes for various actions
   /// such as in a widget.
-  /// [artist] The name of the artist
-  /// [title]  The title of the song
-  /// [image]  The url path to the thumbnail for the song
-  /// [trackURL] The url to the path to extract the songs url
+
   const Song({
     required this.artist,
     required this.title,
     required this.image,
     required this.trackURL,
+    this.streamURL,
+    this.genre,
+    this.dateAdded,
+    this.size,
   });
 
+  /// [artist] The name of the artist
   final String artist;
+
+  /// [title]  The title of the song
   final String title;
+
+  /// [image]  The url path to the thumbnail for the song
   final String image;
+
+  /// [trackURL] The url to the path to extract the songs url
   final String trackURL;
-}
 
-class Track {
-  /// [Track] stores attributes of the songs url to the file
-  /// [url] the file url for the song.
-  const Track({
-    required this.url,
-  });
+  /// [streamURL] The url to the file path on the server
+  final String? streamURL;
 
-  final String url;
+  /// [genre] The genre the song belongs to
+  final String? genre;
+
+  /// [size] The size of the song in megabytes
+  final String? size;
+
+  /// [dateAdded] The date the song was added to the site
+  final String? dateAdded;
 }
