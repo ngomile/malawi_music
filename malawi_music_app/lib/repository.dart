c6cbdde9e4fe29b7dec85b8ef187c28cbf07035d
ngomile <ngomile932@gmail.com>
2022-04-17 09:51:32 +0200

    Implemented base repository methods

--- malawi_music_app/lib/repository.dart
+++ malawi_music_app/lib/repository.dart
@@ -1,3 +1,9 @@
-class SongRepository {}
+import 'package:malawi_music_app/models.dart';
 
-class TrackRepository {}
+class SongRepository {
+  static Future<List<Song>> getSongs(int page) async {}
+}
+
+class TrackRepository {
+  static Future<Track> getTrack(String trackURL) async {}
+}
