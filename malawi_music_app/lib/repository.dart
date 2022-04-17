9cc4e61b969d81045ab2f748a496d61b453242b2
ngomile <ngomile932@gmail.com>
2022-04-17 12:39:25 +0200

    Added comment to getSongs

--- malawi_music_app/lib/repository.dart
+++ malawi_music_app/lib/repository.dart
@@ -1,6 +1,10 @@
 import 'package:malawi_music_app/models.dart';
 
 class SongRepository {
+  /// Performs a get request to the base url path with
+  /// the given [page] argument appended to it, and
+  /// returns a promise that is eventually fulfilled
+  /// to a list of song models.
   static Future<List<Song>> getSongs(int page) async {}
 }
 
