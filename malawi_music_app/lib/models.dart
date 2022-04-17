211a9a5109fa245f6752882290ffc1404f30ec5f
ngomile <ngomile932@gmail.com>
2022-04-17 12:57:53 +0200

    Added comments to model classes

--- malawi_music_app/lib/models.dart
+++ malawi_music_app/lib/models.dart
@@ -1,4 +1,11 @@
 class Song {
+  /// [Song] stores common attributes of songs which can later
+  /// be accessed to use the attributes for various actions
+  /// such as in a widget.
+  /// [artist] The name of the artist
+  /// [title]  The title of the song
+  /// [image]  The url path to the thumbnail for the song
+  /// [trackURL] The url to the path to extract the songs url
   const Song({
     required this.artist,
     required this.title,
@@ -13,6 +20,8 @@ class Song {
 }
 
 class Track {
+  /// [Track] stores attributes of the songs url to the file
+  /// [url] the file url for the song.
   const Track({
     required this.url,
   });
