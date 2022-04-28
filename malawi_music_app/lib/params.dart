58de8636b3cfabc81fba391b31236729a3916d99
ngomile <ngomile932@gmail.com>
2022-04-28 12:12:47 +0200

    Change argument name from track to title

--- malawi_music_app/lib/params.dart
+++ malawi_music_app/lib/params.dart
@@ -1,16 +1,16 @@
 class PlayParams {
   const PlayParams({
-    required this.track,
+    required this.title,
     required this.uri,
   });
 
-  final String track;
+  final String title;
   final String uri;
 
   factory PlayParams.fromMap(Map<String, String> args) {
     final track = args['track'] as String;
     final uri = args['uri'] as String;
 
-    return PlayParams(track: track, uri: uri);
+    return PlayParams(title: track, uri: uri);
   }
 }
