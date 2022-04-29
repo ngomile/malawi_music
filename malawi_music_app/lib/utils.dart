9b8f4c50802673e120c88ed4e1a21a0127bd9c1a
ngomile <ngomile932@gmail.com>
2022-04-29 09:57:48 +0200

    Add type to url parameter

--- malawi_music_app/lib/utils.dart
+++ malawi_music_app/lib/utils.dart
@@ -3,7 +3,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 const kPlaceHolder =
     'https://www.malawi-music.com/timthumb.php?src=http://www.malawi-music.com/images/albums/1650914592_NYimbo.jpg&w=650&q=90';
 
-CachedNetworkImageProvider getCachedImage(url) {
+CachedNetworkImageProvider getCachedImage(String url) {
   try {
     return CachedNetworkImageProvider(url);
   } catch (_) {
