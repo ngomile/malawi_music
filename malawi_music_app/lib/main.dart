63be6dde0612680b9d5e0cba4769d47a2cc47df3
ngomile <ngomile932@gmail.com>
2022-05-03 12:22:05 +0200

    Add initialisation fields to class and event listeners

--- malawi_music_app/lib/main.dart
+++ malawi_music_app/lib/main.dart
@@ -323,105 +323,106 @@ class _PlayPageState extends State<PlayPage> {
           color: Color(0xFFF8F8F8),
         ),
         child: FutureBuilder<Song>(
-            future: fetchSong(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (snapshot.hasError) {
-                return const Center(
-                  child: Text('Failed to load song'),
-                );
-              }
-
-              final song = snapshot.data as Song;
-
-              return Stack(
-                children: [
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    child: AppBar(
-                      leading: IconButton(
-                        onPressed: () => Navigator.pop(context),
-                        icon: const Icon(
-                          Icons.chevron_left,
-                          size: 36,
-                        ),
-                      ),
-                      backgroundColor: Colors.black,
-                      elevation: 0,
-                    ),
-                  ),
-                  Positioned(
-                    top: screenHeight * .12,
-                    left: (screenWidth - 375) / 2,
-                    right: (screenWidth - 375) / 2,
-                    height: screenHeight * .40,
-                    child: CachedNetworkImage(
-                      imageUrl: song.image,
-                      imageBuilder: (context, imageProvider) => Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          color: const Color(0xFF333436),
-                          image: DecorationImage(
-                            image: imageProvider,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      placeholder: (context, url) => Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          color: const Color(0xFF333436),
-                        ),
-                      ),
-                      errorWidget: (context, url, error) => Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          color: const Color(0xFF333436),
-                        ),
-                        child: const Icon(
-                          Icons.error,
-                          size: 36.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: screenHeight * .56,
-                    left: (screenWidth * .10) / 2,
-                    right: (screenWidth * .10) / 2,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          song.title,
-                          style: const TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.w900,
-                            overflow: TextOverflow.visible,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 15),
-                        Text(
-                          song.artist,
-                          style: const TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xFFA6A6A6),
-                          ),
-                        ),
-                        TrackPlayer(uri: song.stream as String),
-                      ],
-                    ),
-                  ),
-                ],
+          future: fetchSong(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
               );
-            }),
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('Failed to load song'),
+              );
+            }
+
+            final song = snapshot.data as Song;
+
+            return Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: AppBar(
+                    leading: IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: const Icon(
+                        Icons.chevron_left,
+                        size: 36,
+                      ),
+                    ),
+                    backgroundColor: Colors.black,
+                    elevation: 0,
+                  ),
+                ),
+                Positioned(
+                  top: screenHeight * .12,
+                  left: (screenWidth - 375) / 2,
+                  right: (screenWidth - 375) / 2,
+                  height: screenHeight * .40,
+                  child: CachedNetworkImage(
+                    imageUrl: song.image,
+                    imageBuilder: (context, imageProvider) => Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: const Color(0xFF333436),
+                        image: DecorationImage(
+                          image: imageProvider,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    placeholder: (context, url) => Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: const Color(0xFF333436),
+                      ),
+                    ),
+                    errorWidget: (context, url, error) => Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: const Color(0xFF333436),
+                      ),
+                      child: const Icon(
+                        Icons.error,
+                        size: 36.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: screenHeight * .56,
+                  left: (screenWidth * .10) / 2,
+                  right: (screenWidth * .10) / 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        song.title,
+                        style: const TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w900,
+                          overflow: TextOverflow.visible,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 15),
+                      Text(
+                        song.artist,
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFFA6A6A6),
+                        ),
+                      ),
+                      TrackPlayer(uri: song.stream as String),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
@@ -442,6 +443,35 @@ class TrackPlayer extends StatefulWidget {
 }
 
 class _TrackPlayerState extends State<TrackPlayer> {
+  late AudioPlayer _advancedPlayer = AudioPlayer();
+
+  Duration _duration = Duration();
+  Duration _position = Duration();
+
+  bool _isPlaying = false;
+
+  final List<IconData> _icons = [
+    Icons.play_circle_filled,
+    Icons.pause_circle_filled,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _advancedPlayer.onDurationChanged.listen(
+      (duration) => setState(() {
+        _duration = duration;
+      }),
+    );
+
+    _advancedPlayer.onAudioPositionChanged.listen((duration) => setState(() {
+          _position = duration;
+        }));
+
+    _advancedPlayer.setUrl(widget.uri);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
