9a980566963e7d140a505accaa393516928e7f32
ngomile <ngomile932@gmail.com>
2022-05-14 23:23:32 +0200

    Added icon button to navigate to latest page

--- malawi_music_app/lib/ui/headers.dart
+++ malawi_music_app/lib/ui/headers.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:malawi_music_app/ui/styles.dart';
+import 'package:malawi_music_app/ui/ui.dart';
 
 class LatestSongHeader extends StatelessWidget {
   const LatestSongHeader({
@@ -8,24 +9,44 @@ class LatestSongHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 16.0),
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: const [
-          Text(
-            'Latest',
-            style: kHeaderFontStyle,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 16.0),
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: const [
+              Text(
+                'Latest',
+                style: kHeaderFontStyle,
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'Explore the latest music',
+                style: kHeaderSubFontStyle,
+              ),
+            ],
           ),
-          SizedBox(height: 8.0),
-          Text(
-            'Explore the latest music',
-            style: kHeaderSubFontStyle,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8.0,
+            vertical: 16.0,
           ),
-        ],
-      ),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.chevron_right_rounded,
+              color: kPrimaryLightColor,
+              size: 32.0,
+              semanticLabel: 'Browse Latest',
+            ),
+          ),
+        )
+      ],
     );
   }
 }
