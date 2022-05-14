4db7eda5a9c0aafb01809af1e36faf6e24998939
ngomile <ngomile932@gmail.com>
2022-05-14 15:55:06 +0200

    Refactor headers into separate file

--- malawi_music_app/lib/ui/pages/home_page.dart
+++ malawi_music_app/lib/ui/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:malawi_music_app/args.dart';
 import 'package:malawi_music_app/models.dart';
 import 'package:malawi_music_app/repository.dart';
-import 'package:malawi_music_app/styles.dart';
+import 'package:malawi_music_app/ui/headers.dart';
 import 'package:malawi_music_app/ui/ui.dart';
 
 class HomePage extends StatefulWidget {
@@ -33,35 +33,6 @@ class LatestSection extends StatelessWidget {
     return const SectionContainer(
       heading: LatestSongHeader(),
       child: LatestSongsList(),
-    );
-  }
-}
-
-class LatestSongHeader extends StatelessWidget {
-  const LatestSongHeader({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 16.0),
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: const [
-          Text(
-            'Latest',
-            style: kHeaderFontStyle,
-          ),
-          SizedBox(height: 8.0),
-          Text(
-            'Explore the latest music',
-            style: kHeaderSubFontStyle,
-          ),
-        ],
-      ),
     );
   }
 }
