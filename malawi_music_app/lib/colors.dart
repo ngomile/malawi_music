02de69d43cb6187f03b9b3644771495d96ad3d1b
ngomile <ngomile932@gmail.com>
2022-05-14 15:40:38 +0200

    Place lead and sub header text styles in separate file

--- malawi_music_app/lib/colors.dart
+++ malawi_music_app/lib/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kImageBGColor = Color(0xFF333436);
 const kLightFontColor = Color(0xFFF8F8F8);
 const kDarkBGColor = Color(0xFF000000);
+const kSubFontColor = Color(0xFF808080);
---
+++ malawi_music_app/lib/styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:malawi_music_app/colors.dart';
+
+const kHeaderFontStyle = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.bold,
+);
+
+const kHeaderSubFontStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w600,
+  color: kSubFontColor,
+);
