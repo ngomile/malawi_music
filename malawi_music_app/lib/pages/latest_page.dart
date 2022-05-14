b1d19447599660541b38bd3f1d7b091d83ad3c0e
ngomile <ngomile932@gmail.com>
2022-05-15 00:12:06 +0200

    Implemented UI logic for latest_page

--- malawi_music_app/lib/pages/latest_page.dart
+++ malawi_music_app/lib/pages/latest_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:malawi_music_app/ui/ui.dart';
 
 class LatestPage extends StatelessWidget {
   const LatestPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MainContent(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(
+            Icons.chevron_left,
+            size: 32.0,
+          ),
+        ),
+        title: const Text(
+          'Latest',
+          style: TextStyle(
+            fontSize: 36.0,
+          ),
+        ),
+      ),
+      child: Container(),
+    );
   }
 }
