9d1e68750ea088d5c6d2410cac22230ddf706af4
ngomile <ngomile932@gmail.com>
2022-05-14 19:51:03 +0200

    Use PaginatedBuilder in LatestSongList widget

--- malawi_music_app/lib/ui/lists.dart
+++ malawi_music_app/lib/ui/lists.dart
@@ -23,9 +23,8 @@ class _LatestSongsListState extends State<LatestSongsList> {
     super.initState();
     _streamController ??= StreamController.broadcast();
 
-    _streamController?.stream.listen(
-      (song) => setState(() => _songs.add(song)),
-    );
+    _streamController?.stream
+        .listen((data) => setState(() => _songs.add(data)));
     fetchSongs();
   }
 
@@ -38,9 +37,6 @@ class _LatestSongsListState extends State<LatestSongsList> {
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final orientation = MediaQuery.of(context).orientation;
-
     return Container(
       padding: const EdgeInsets.symmetric(
         vertical: 16.0,
@@ -54,90 +50,97 @@ class _LatestSongsListState extends State<LatestSongsList> {
         ),
       ),
       margin: const EdgeInsets.only(top: 12.0),
-      child: StreamBuilder<Song>(
+      child: PaginatedBuilder<Song>(
+        builder,
         stream: _streamController?.stream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+      ),
+    );
+  }
 
-          return ListView.builder(
-            itemCount: _songs.length,
-            itemBuilder: (context, index) {
-              final song = _songs.elementAt(index);
+  ///[builder] gets called after a successful stream operation, returning the
+  ///widget that displays the result from the stream
+  Widget builder(BuildContext context, AsyncSnapshot<Song?> snapshot) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final orientation = MediaQuery.of(context).orientation;
 
-              return Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(2.0),
-                      bottomLeft: Radius.circular(2.0)),
-                  // color: Colors.grey,
+    if (!snapshot.hasData) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: _songs.length,
+      itemBuilder: (context, index) {
+        final song = _songs.elementAt(index);
+
+        return Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(2.0),
+                bottomLeft: Radius.circular(2.0)),
+            // color: Colors.grey,
+          ),
+          height: orientation == Orientation.portrait
+              ? screenHeight * .10
+              : screenHeight * .15,
+          margin: const EdgeInsets.only(bottom: 24.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                width: 96.0,
+                margin: const EdgeInsets.only(right: 16.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100.0),
+                  child: CachedImage(song.image),
                 ),
-                height: orientation == Orientation.portrait
-                    ? screenHeight * .10
-                    : screenHeight * .15,
-                margin: const EdgeInsets.only(bottom: 24.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Container(
-                      width: 96.0,
-                      margin: const EdgeInsets.only(right: 16.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100.0),
-                        child: CachedImage(song.image),
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.max,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            GestureDetector(
-                              onTapUp: (_) => Navigator.pushNamed(
-                                context,
-                                '/play',
-                                arguments: PlayArgs(
-                                  title: song.title,
-                                  uri: song.track,
-                                ),
-                              ),
-                              child: Text(
-                                song.title,
-                                style: const TextStyle(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.w700,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 4.0),
-                            Text(
-                              song.artist,
-                              style: const TextStyle(
-                                overflow: TextOverflow.ellipsis,
-                                fontSize: 18,
-                                color: Color(0xFFA6A6A6),
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      GestureDetector(
+                        onTapUp: (_) => Navigator.pushNamed(
+                          context,
+                          '/play',
+                          arguments: PlayArgs(
+                            title: song.title,
+                            uri: song.track,
+                          ),
+                        ),
+                        child: Text(
+                          song.title,
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 4.0),
+                      Text(
+                        song.artist,
+                        style: const TextStyle(
+                          overflow: TextOverflow.ellipsis,
+                          fontSize: 18,
+                          color: Color(0xFFA6A6A6),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              );
-            },
-          );
-        },
-      ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 
